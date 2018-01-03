db9fa1b816fc67744335ae0940e81491f4b4b8c9
Robbert <hofman.robbert@gmail.com>
2018-01-03 17:37:23 +0100

    wrote character, player and food classes. Much more scalable code. Got rid of some jquery along the way.

--- public/tn/game.js
+++ public/tn/game.js
@@ -1,60 +1,142 @@
-var player = {x: $(window).width()/2, y: $(window).height()/2, fontsize: 12}
-var food = {x: 50, y: 50}
-var action = false;
+// GAME PARAMETERS 
 var fadeTime = 20;
 var stepSize = 50;
 var threshold = stepSize;
 
+// ENUMS 
+var directions = Object.freeze({LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40}); // safe enum
+
 // TODO:
-// enum directions
-// make abstracter definitions of things like move(character, direction) 
 // add monster & collision detection for monster (can also eat food)
-// find good practice for JS with JQuery (DOM): edit style of element or its corresponding class?
+// find good practice for JS with JQuery (DOM?): edit style of element or its corresponding class?
 // add score counter along with controls (increase amount of monsters/ foods)
 // fix collision detection for div size instead of upperleft point
 // substitute text for low-res pictures (or not? :p)
 // add diversity (banana, cherry, apple)
+// multiplayer with other key combinations!
+// enemies should not entirely randomwalk, they should be pushed towards player as well
 
+// CLASS DEFINITIONS (js hoisting)
+class Character {
+	constructor(x=0, y=0, name="") {
+		this.htmlElement = document.createElement("div"); // characters are divs
+		this.htmlElement.style.position = "fixed";
+		this.setName(name);
+		this.setX(x);
+		this.setY(y);
+		document.body.appendChild(this.htmlElement);
+	}
+
+	getX() {
+		return this.htmlElement.style.left;
+	}
+
+	getY() {
+		return this.htmlElement.style.top;	
+	}
+
+	setX(x) {
+		this.x = x; // need to be able to access the integer as well
+		this.htmlElement.style.left = this.x + "px";
+	}
+
+	setY(y) {
+		this.y = y;
+		this.htmlElement.style.top = this.y + "px";
+	}
+
+	setName(name) {
+		this.htmlElement.innerHTML = name;
+	}
+
+	move(direction, amount) {
+		switch (direction) {
+			case directions.LEFT:
+				this.setX(this.x - amount);
+				break;
+			case directions.UP:
+				this.setY(this.y - amount);
+				break;
+			case directions.RIGHT:
+				this.setX(this.x + amount);
+				break;
+			case directions.DOWN:
+				this.setY(this.y + amount);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+class Monster extends Character {
+	constructor(x,y){
+		super(x,y);
+	}
+}
+
+class Player extends Character {
+	constructor(x, y, name, fontSize=12){
+		super(x, y, name); // calls superclass' constructor
+		this.setFontSize(fontSize);
+	}
+
+	setFontSize(fontSize) {
+		this.fontSize = fontSize; // need to keep integer as well
+		this.htmlElement.style.fontSize = this.fontSize + "px";
+	}
+
+	setColor(color) {
+		this.htmlElement.style.color = color;
+	}
+
+	toggleFontWeight() {
+		this.htmlElement.style.fontWeight = "bold";
+	}
+}
+
+class Food extends Character {
+	constructor(x=0, y=0, name="") {
+		super(x, y, name);
+	}
+}
+
+// GAME
 $(document).ready(function(){
 	console.log("document ready");
-	initializeArrows();
-	document.getElementById("enemy").style.display = "none";
+	initializeArrows(); // should only set these guys up once
 	console.log("setup complete");
 });
 
 $(document).on('keyup', function(e){
 	checkButtonPress(e);
 	collisionDetection();
-	writePositions();
 });
+
+var player = new Player(100, 100, "toonisnemiet", 19);
+var food = new Food(200, 50, "banaan");
 
 function initializeArrows(){
 	var arrowLeft = document.getElementById("arrowLeft");
 	var arrowRight = document.getElementById("arrowRight");
 	var arrowUp = document.getElementById("arrowUp");
 	var arrowDown = document.getElementById("arrowDown");
-	arrowLeft.style.cursor = "pointer";
-	arrowRight.style.cursor = "pointer";
-	arrowUp.style.cursor = "pointer";
-	arrowDown.style.cursor = "pointer";
-	arrowLeft.onclick = function(){player.x -= stepSize; writePositions(); collisionDetection();};
-	arrowRight.onclick = function(){player.x += stepSize; writePositions(); collisionDetection();};
-	arrowUp.onclick = function(){player.y -= stepSize; writePositions(); collisionDetection();};
-	arrowDown.onclick = function(){player.y += stepSize; writePositions(); collisionDetection();};
-}
-
-function writePositions(){
-	$('.player').css({'left': player.x, 'top': player.y}); // always write player position
-	$('.food').css({'left': food.x, 'top': food.y}); // always write food position 
+	var arrows = [arrowLeft, arrowUp, arrowRight, arrowDown];
+	for (var i = 0; i < arrows.length; i++) {
+		arrows[i].style.cursor = "pointer";	
+	}
+	arrowLeft.onclick = function(){player.move(directions.LEFT, stepSize); collisionDetection();};
+	arrowRight.onclick = function(){player.move(directions.RIGHT, stepSize); collisionDetection();};
+	arrowUp.onclick = function(){player.move(directions.UP, stepSize); collisionDetection();};
+	arrowDown.onclick = function(){player.move(directions.DOWN, stepSize); collisionDetection();};
 }
 
 function collisionDetection(){
 	if (Math.abs(player.x - food.x) < threshold && 
 		Math.abs(player.y - food.y) < threshold) { // objects are close enough: collision
-		player.fontsize += 3;
-		$('.player').css({'font-size': player.fontsize}); // adjust fontsize
-		food.x = Math.random()*$(window).width(); // spawn food somewhere else
-		food.y = Math.random()*$(window).height();
+		player.setFontSize(player.fontSize + 3);
+		food.setX(Math.random()*$(window).width()); // spawn food somewhere else
+		food.setY(Math.random()*$(window).height());
 	}
 }
 
@@ -62,50 +144,25 @@ function checkButtonPress(e){
 	console.log(e.which); // check which button is being pressed
 	switch (e.which) {
 		case 37: // left
-			player.x -= stepSize;
-			action = true;
-			break;
-		case 39: // right
-			player.x += stepSize;
-			action = true;
+			player.move(directions.LEFT, stepSize);
 			break;
 		case 38: // up
-			player.y -= stepSize; 
-			action = true;
+			player.move(directions.UP, stepSize);
+			break;
+		case 39: // right
+			player.move(directions.RIGHT, stepSize);
 			break;
 		case 40: // down
-			player.y += stepSize;
-			action = true;
+			player.move(directions.DOWN, stepSize);
 			break;
 		case 66: // b
-			$('.player').css({
-				'font-weight': 'bold'
-			});
+			player.toggleFontWeight();
 			break;
 		case 82: // r
-			$('.player').css({
-				'color': 'red'
-			});
+			player.setColor("red");	
 			break;
 		default: // default
-			action = false;
 			break;
-	}
-	if (action) {
-		$('.player').fadeOut(fadeTime).css({
-			'left': player.x,
-			'top': player.y
-		}).fadeIn(fadeTime);
 	}
 }
 
-function randomWalk(character){
-	newx = character.x + math.floor(Math.random()*stepSize);
-	newy = character.y + math.floor(Math.random()*stepSize);
-	if (newx < $(window).width() && newx > 0){
-		character.x = newx;
-	}
-	if (newy < $(window).height() && newy > 0){
-		character.y = newy;
-	}
-}
