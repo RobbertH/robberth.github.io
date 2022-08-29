efeb12149b4d735e9cde11d1eedce963c65d9841
robbert <robberth>
2022-08-29 21:48:38 +0200

    somewhat more acceptable crackAndGo script

--- _includes/other/crackAndGo.js
+++ _includes/other/crackAndGo.js
@@ -1,33 +1,26 @@
 // ==UserScript==
-// @name         Crack & Go
+// @name         Collect & Go
 // @version      0.1
-// @description  try to take over the world!
+// @description  Script to fetch ingredients from Colruyt mealboxes and add them to cart on Collect and Go
 // @author       You
 // @match        https://colruyt.collectandgo.be/*
 // @grant        https://colruyt.collectandgo.be/*
 // ==/UserScript==
 
-var URL_MODIFIER = "tntntn"; // this can be anything that colruyt doesn't recognize as a parameter
+'use strict';
 
-var extract_ingredients = function() {
-    'use strict'; // never use unitialized variables,...
+let URL_MODIFIER = "tntntn"; // this can be anything that colruyt doesn't recognize as a parameter
 
-    console.log("CRACK & GO - Extracting ingredients -----------------------------------------");
+let extract_ingredients = function() {
+    console.log("Extracting ingredients");
 
-    var recipe__ingredients = document.getElementsByClassName("recipe__ingredients");
-    console.log(recipe__ingredients);
-    var ingredients = recipe__ingredients[0].children[1].children;
-    console.log(ingredients);
+    let recipe__ingredients = document.getElementsByClassName("recipe__ingredients");
+    let ingredients = recipe__ingredients[0].children[1].children;
     console.log(ingredients.length);
-    var i;
-    for (i=0; i<ingredients.length; i++){
-        console.log(ingredients[i].innerText);
-    }
+    console.log("Opening new tab per ingredient");
 
-    console.log("CRACK & GO - Opening new tab per ingredient ------------------------------------------");
-
-    var urlString;
-    for (i=0; i<ingredients.length; i++){
+    let urlString;
+    for (let i=0; i<ingredients.length; i++){
         urlString = 'https://colruyt.collectandgo.be/cogo/nl/zoeken?z=';
         urlString += ingredients[i].innerText;
         urlString += '&category=generic';
@@ -36,33 +29,30 @@ var extract_ingredients = function() {
         window.open(urlString, '_blank'); // _blank means open in new tab
     }
 
-    console.log("CRACK & GO - Ingredient extraction done, continue on other tabs ------------------------------------------");
+    console.log("Ingredient extraction done");
 
 }
 
-var buy_ingredient = function() {
-    'use strict';
+let buy_ingredient = function() {
+    console.log("Buying ingredient");
 
-    console.log("CRACK & GO - Buying ingredient --------------------------");
-
-
-  	var articles = document.querySelector("#articles").firstChild.firstChild; // buy first item once
-  	console.log("CRACK & GO - Got articles: ---------- ");
+  	let articles = document.querySelector("#articles").firstChild.firstChild; // buy first item once
+  	console.log("Got articles:");
   	console.log(articles);
     articles.lastChild.lastChild.firstChild.firstChild.submit()
     window.close();
 
-    console.log("CRACK & GO - Done buying ingredient --------------------------");
+    console.log("Done buying ingredient");
 }
 
-var main = function() {
+let main = function() {
   console.log("Running greasemonkey script!");
 
-  var url = window.location.href;
+  let url = window.location.href;
   if (url.indexOf("recipedetail") > -1) { // URL contains "recipedetail"
     extract_ingredients();
   }
-  if (url.indexOf(URL_MODIFIER) > -1) { // URL was modified by Crack & Go script
+  if (url.indexOf(URL_MODIFIER) > -1) { // URL was modified by this script
   	buy_ingredient();
   }
 
